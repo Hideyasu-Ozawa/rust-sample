a286c27a544fa6958ae0feec47635ad3f4378bc4
Hideyasu-Ozawa <ozawa.hideyasu@isid.co.jp>
2022-05-12 00:39:29 +0900

    add a workflow

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
-use rand::Rng;
+// use rand::Rng;
+fn simple_branch(flg: bool) -> String{
+    if flg {
+        "true branch".to_string()
+    } else {
+        "false branch".to_string()
+    }
+}
 
 fn main() {
     println!("Hello, world!");
 
-    let mut rng = rand::thread_rng();
-    let i: i32 = rng.gen();
-    println!("i = {}", i);
+    // let mut rng = rand::thread_rng();
+    // let i: i32 = rng.gen();
+    // println!("i = {}", i);
+
+    let flg = false;
+
+    simple_branch(flg);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn true_branch() {
+        assert_eq!("true branch", simple_branch(true));
+    }
+
+    // #[test]
+    // fn false_branch() {
+    //     assert_eq!("false branch", simple_branch(false));
+    // }
 }
